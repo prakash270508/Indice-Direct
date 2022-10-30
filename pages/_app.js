f46938d94636cd49b909a753dbad040f7c8cf2d5
prakash270508 <112390221+prakash270508@users.noreply.github.com>
2022-10-30 17:39:20 +0530

    Initial Commit

---
+++ pages/_app.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { IndiceProvider } from '../contexts';
+
+import '../styles/bootstrap.min.css';
+import '../styles/animate.min.css';
+import '../styles/boxicons.min.css';
+import '../styles/flaticon.css';
+import "swiper/css";
+import "swiper/css/bundle";
+
+// Global Style
+import '../styles/style.css';
+import '../styles/responsive.css';
+
+import Layout from '../components/_App/Layout';
+import Loader from '../components/Shared/Loader';
+import GoTop from '../components/Shared/GoTop';
+import { SnackbarProvider } from 'notistack';
+
+
+function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 2000);
+  }, []);
+
+  return (
+    <>
+    <SnackbarProvider maxSnack={3}>
+        <Layout>
+            <IndiceProvider>
+
+                <Component {...pageProps} />
+
+                <Loader loading={loading} />
+
+                <GoTop scrollStepInPx='100' delayInMs='10.50' />
+
+            </IndiceProvider>
+        </Layout>
+    </SnackbarProvider>
+
+    </>
+  );
+}
+
+export default MyApp;
